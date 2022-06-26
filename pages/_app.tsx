121fa00e4c4ee1092bd84646dee92b770a37cb49
zakiego <belummikir@gmail.com>
2022-06-27 01:20:56 +0800

    feat: add comment _app.tsx getLayout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import { AppProps } from "next/app";
 import "nextra-theme-docs/style.css";
 import { ReactElement, ReactNode } from "react";
 
+// https://nextjs.org/docs/basic-features/layouts#with-typescript
+
 export type NextPageWithLayout = NextPage & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
